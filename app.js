792a01eab3bac01c29d1ddaef442068bfbe3ba45
Max Cramer <maximiliancramer@yahoo.com>
2019-09-20 15:51:33 +0100

    put logic into one .ready function

--- app.js
+++ app.js
@@ -22,26 +22,6 @@ var data = [
   }
 ];
 
-var bootData = [
-  {
-    'title': 'Boot Title 1',
-    'button': 'Discover 1',
-    'description': 'Description 1',
-    'img': 'Images/Boot1.jpg'
-  },
-  {
-    'title': 'Boot Title 2',
-    'button': 'Discover 2',
-    'description': 'Description 2',
-    'img': 'Images/Boot2.jpg'
-  },
-  {
-    'title': 'Boot Title 3',
-    'button': 'Discover 3',
-    'description': 'Description 3',
-    'img': 'Images/Boot3.jpg'
-  }
-];
 
 var image__places = [
   '#second__image',
@@ -49,21 +29,16 @@ var image__places = [
   '#fourth__image'
 ];
 
-var boot__desc = [
-  '#first_description',
-  '#second_description',
-  '#thrid_description'
-];
 
-$( document ).ready(function () {
-  var onLoadImage = '<img src="Images/Boot1.jpg"/>';
-  $('#main__image').html(onLoadImage);
-});
-
-$( document ).ready(function () {
-  var onLoadDescription = '<h2>Boot Title 1</h2><button>Discover 1</button><h5>Description 1</h5>';
-  $('#info').html(onLoadDescription);
-});
+// $( document ).ready(function () {
+//   var onLoadImage = '<img src="Images/Boot1.jpg"/>';
+//   $('#main__image').html(onLoadImage);
+// });
+//
+// $( document ).ready(function () {
+//   var onLoadDescription = '<h2>Boot Title 1</h2><button>Discover 1</button><h5>Description 1</h5>';
+//   $('#info').html(onLoadDescription);
+// });
 
 
 // TRYING TO DISPLAY DESCRIPTIONS
@@ -85,34 +60,52 @@ $( document ).ready(function () {
 //   }
 // });
 //
-$(document).ready(function () {
+// $(document).ready(function () {
+//   var onLoadImage = '<img src="Images/Boot1.jpg"/>';
+//   $('#main__image').html(onLoadImage);
+//
+//   var onLoadDescription = '<h2>Boot Title 1</h2><button>Discover 1</button><h5>Description 1</h5>';
+//   $('#info').html(onLoadDescription);
+//
+//   for(var i =0; i < bootData.length; i++){
+//     var title = '<h2>'+bootData[0].title+'</h2>';
+//     var button = '<button>'+bootData[0].button+'</button>';
+//     var description = '<h5>'+bootData[0].description+'</h5>';
+//
+//     $(boot__desc[i]).html(title);
+//     $(boot__desc[i]).html(button);
+//     $(boot__desc[i]).html(description);
+//
+//     $(boot__desc[i]).on('click', function(e){
+//       var imageIClickedOn = e.target.outerHTML;
+//       $('#info').html(imageIClickedOn);
+//     });
+//   }
+// });
 
-  for(var i =0; i < bootData.length; i++){
-    var title = '<h2>'+bootData[0].title+'</h2>';
-    var button = '<button>'+bootData[0].button+'</button>';
-    var description = '<h5>'+bootData[0].description+'</h5>';
+$( document ).ready(function () {
 
-    $(boot__desc[i]).html(title);
-    $(boot__desc[i]).html(button);
-    $(boot__desc[i]).html(description);
+  var onLoadImage = '<img src="Images/Boot1.jpg"/>';
+  $('#main__image').html(onLoadImage);
 
-    $(boot__desc[i]).on('click', function(e){
+  var onLoadDescription = '<h2>Boot Title 1</h2><button>Discover 1</button><h5>Description 1</h5>';
+  $('#info').html(onLoadDescription);
+
+  for(var i =0; i < data.length; i++){
+    var title = '<h2>'+data[0].title+'</h2>';
+    var button = '<button>'+data[0].button+'</button>';
+    var description = '<h5>'+data[0].description+'</h5>';
+
+    $(image__places[i]).html(title);
+    $(image__places[i]).html(button);
+    $(image__places[i]).html(description);
+
+    $(image__places[i]).on('click', function(e){
       var imageIClickedOn = e.target.outerHTML;
       $('#info').html(imageIClickedOn);
     });
   }
-});
 
-$( document ).ready(function () {
-  var image = '<img src="'+data[0].img+'" />';
-  // var secondImage = '<img src ="Images/Boot1.jpg"/>';
-  // var thirdImage = '<img src ="Images/Boot2.jpg"/>';
-  // var fourthImage = '<img src ="Images/Boot3.jpg"/>';
-  // $("#main__image").html(image);
-  // $("#second__image").html(secondImage);
-  // $("#third__image").html(thirdImage);
-  // $("#fourth__image").html(fourthImage);
-  //$('#main__image').html(image);
   for(var i = 0; i < data.length; i++){
     var image = '<img src="'+data[i].img+'" alt=""/>';
     $(image__places[i]).html(image);
